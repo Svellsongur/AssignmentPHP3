a198410bc9f0bc7678147792e898adab7fb42258
Vu Chi Doanh <vuchidoanh2003@gmail.com>
2023-07-28 03:59:14 +0700

    add view

---
+++ resources/views/templates/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('page_title')</title>
+    @yield('page_css')
+    @yield('head_js')
+
+</head>
+
+<body>
+    @yield('header')
+    @yield('content')
+    @yield('footer')
+
+
+
+    @yield('page_js')
+
+</body>
+
+</html>
