935a2f48992d7f3ee0b24c2455b1614dadd04002
Vu Chi Doanh <vuchidoanh2003@gmail.com>
2023-07-28 18:22:20 +0700

    add login form

---
+++ resources/views/default/login.blade.php
@@ -0,0 +1,45 @@
+@extends('templates.layout')
+
+@section('page_title')
+Login
+@endsection
+
+<!-- link thư viện JS -->
+@section('head_js')
+@endsection
+
+<!-- gọi admin header -->
+@section('header')
+@include('default.header')
+@endsection
+
+<!-- Show content -->
+@section('content')
+<div class="container">
+    @auth
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input type="email" name="loginemail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" name="loginpassword" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">Đăng nhập</button>
+        <p>Chưa có tài khoản? <a href="">Đăng ký</a></p>
+    </form>
+    @endauth
+</div>
+@endsection
+
+
+<!-- gọi admin footer -->
+@section('footer')
+@include('default.footer')
+@endsection
+
+<!-- JS nội tuyến -->
+@section('page_js')
+@endsection
